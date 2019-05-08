0a209bdca3c2fe13d3744e25aa404a756ade9053
Jason Felds <jafelds@gmail.com>
2019-05-08 16:50:02 -0400

    Start with what's been done.

---
+++ scripts/init.lua
@@ -0,0 +1,9 @@
+-- Load the items
+Tracker:AddItems( "items/equipment.json" )
+Tracker:AddItems( "items/emblems.json" )
+Tracker:AddItems( "items/inventory.json" )
+Tracker:AddItems( "items/souls.json" )
+Tracker:AddItems( "items/stones.json" )
+
+Tracker:AddLayouts( "layouts/tracker.json" )
+Tracker:AddLayouts( "layouts/broadcast.json" )
