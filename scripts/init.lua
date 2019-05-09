bfc92a798dc865737aa0757ae77f4ac758c1b5c5
Jason Felds <jafelds@gmail.com>
2019-05-08 22:17:27 -0400

    World 1 almost done.

--- scripts/init.lua
+++ scripts/init.lua
@@ -4,6 +4,11 @@ Tracker:AddItems( "items/emblems.json" )
 Tracker:AddItems( "items/inventory.json" )
 Tracker:AddItems( "items/souls.json" )
 Tracker:AddItems( "items/stones.json" )
+Tracker:AddItems( "items/labels.json" )
+
+Tracker:AddMaps( "maps/maps.json" )
+
+Tracker:AddLocations( "locations/world1.json" )
 
 Tracker:AddLayouts( "layouts/tracker.json" )
 Tracker:AddLayouts( "layouts/broadcast.json" )
