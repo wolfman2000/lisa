d8ccb1c734d824bfa59a067c64f5146521e8226b
Jason Felds <jafelds@gmail.com>
2019-05-11 22:07:56 -0400

    ...we have it.

--- scripts/init.lua
+++ scripts/init.lua
@@ -5,17 +5,26 @@ Tracker:AddItems( "items/inventory.json" )
 Tracker:AddItems( "items/souls.json" )
 Tracker:AddItems( "items/stones.json" )
 
-for i=1,6 do
-  Tracker:AddItems( "items/labels/world" .. i .. ".json" )
+local i = 0
+local j = 0
+i, j = string.find( Tracker.ActiveVariantUID, "map_simple" )
+
+if (i ~= nil and i > 0) then
+  for i=1,6 do
+    Tracker:AddItems( "items/labels/world" .. i .. ".json" )
+  end
+
+  ScriptHost:LoadScript( "scripts/logic.lua" )
+
+  Tracker:AddMaps( "maps/maps.json" )
+
+  for i=1,6 do
+    Tracker:AddLocations( "locations/world" .. i .. ".json" )
+  end
+
+  Tracker:AddLayouts( "layouts/tracker.json" )
+else
+  Tracker:AddLayouts( "layouts/itemonly.json" )
 end
 
-ScriptHost:LoadScript( "scripts/logic.lua" )
-
-Tracker:AddMaps( "maps/maps.json" )
-
-for i=1,6 do
-  Tracker:AddLocations( "locations/world" .. i .. ".json" )
-end
-
-Tracker:AddLayouts( "layouts/tracker.json" )
 Tracker:AddLayouts( "layouts/broadcast.json" )
