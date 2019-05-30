9065494e7b6643269daac83c07bc7dab2e95dec0
Jason Felds <jafelds@gmail.com>
2019-05-29 22:14:16 -0400

    Add variants for layouts for all equipment.

--- scripts/init.lua
+++ scripts/init.lua
@@ -8,6 +8,9 @@ Tracker:AddItems( "items/stones.json" )
 local i = 0
 local j = 0
 i, j = string.find( Tracker.ActiveVariantUID, "map_simple" )
+local x = 0
+local y = 0
+x, y = string.find( Tracker.ActiveVariantUID, "items_equipment" )
 
 if (i ~= nil and i > 0) then
   for i=1,6 do
@@ -22,9 +25,23 @@ if (i ~= nil and i > 0) then
     Tracker:AddLocations( "locations/world" .. i .. ".json" )
   end
 
-  Tracker:AddLayouts( "layouts/tracker.json" )
+  if ( x ~= nil and x > 0 ) then
+    Tracker:AddLayouts( "layouts/tracker_equipment.json" )
+  else
+    Tracker:AddLayouts( "layouts/tracker.json" )
+  end
 else
-  Tracker:AddLayouts( "layouts/itemonly.json" )
+  if ( x ~= nil and x > 0 ) then
+    Tracker:AddLayouts( "layouts/itemonly_equipment.json" )
+  else
+    Tracker:AddLayouts( "layouts/itemonly.json" )
+  end
 end
 
-Tracker:AddLayouts( "layouts/broadcast.json" )
+if ( x ~= nil and x > 0 ) then
+  Tracker:AddLayouts( "layouts/broadcast_equipment.json" )
+else
+  Tracker:AddLayouts( "layouts/broadcast.json" )
+end
+
+
